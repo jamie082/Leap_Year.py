7cb282e1e4fa5ac92e1a92f3fc4934c9a6972fab
jamie082 <jamiemorrissey02@gmail.com>
2023-05-07 18:12:55 -0700

    added working source code

--- leap_year.py
+++ leap_year.py
@@ -1,16 +1,15 @@
-print('Leap year yes or no??')
+print('Leap year yes or no??  v. 001')
 print('By Jamie morrissey')
 
-user_input = input('Enter Input Year: ')
-print('Year is: ' + user_input)
-'''add Python comment'''
+user_input = int(input('Enter Input Year: '))
+print(user_input)
+# add python comment
 
-def start_program():
-    if (year % 400 == 0) and (year % 100 == 0):
-        print("{0} is a leap year".format(year))
+if (user_input % 400 == 0) and (user_input % 100 == 0):
+    print("{0} is a leap year".format(user_input))
 
-    elif (year % 4 == 0) and (year % 100 != 0):
-        print("{0} is a leap year".format(year))
+elif (user_input % 4 == 0) and (user_input % 100 != 0):
+    print("{0} is a leap year".format(user_input))
 
-    else:
-        print("{0} is not a leap year".format(year))
+else:
+    print("{0} is not a leap year".format(user_input))
