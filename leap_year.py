ff343dcda2f3925f99eee52f31da2c62366aecab
jamie082 <jamiemorrissey02@gmail.com>
2023-05-07 17:54:58 -0700

    added source code to distro

--- leap_year.py
+++ leap_year.py
@@ -3,4 +3,14 @@ print('By Jamie morrissey')
 
 user_input = input('Enter Input Year: ')
 print('Year is: ' + user_input)
-''' add Python comment
+'''add Python comment'''
+
+def start_program():
+    if (year % 400 == 0) and (year % 100 == 0):
+        print("{0} is a leap year".format(year))
+
+    elif (year % 4 == 0) and (year % 100 != 0):
+        print("{0} is a leap year".format(year))
+
+    else:
+        print("{0} is not a leap year".format(year))
